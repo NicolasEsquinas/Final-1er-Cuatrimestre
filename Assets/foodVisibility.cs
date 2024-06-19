464870aff69a8bbfbaca87e4048b2865d9ad56a3
NicolasEsquinas <esquinasnico@gmail.com>
2024-06-19 15:30:34 -0300

    Food visibility administrator

--- Assets/foodVisibility.cs
+++ Assets/foodVisibility.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class foodVisibility : MonoBehaviour
 {
+    public GameObject[] food;
+    public int currentIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DesactiveAll();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentIndex++;
+            if (currentIndex < food.Length)
+            {
+                ActivateByIndex(currentIndex);
+
+            }
+            else
+            {
+                currentIndex = 0;
+                DesactiveAll();
+                ActivateByIndex(currentIndex);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentIndex--;
+            if (currentIndex > -1)
+            {
+                DesactivateByIndex(currentIndex);
+
+            }
+            else
+            {
+                currentIndex = food.Length;
+                ActiveAll();
+                ActivateByIndex(currentIndex);
+            }
+        }
+    }
+
+    void DesactiveAll()
+    {
+        for (int i = 0; i < food.Length; i++)
+        {
+            food[i].SetActive(false);
+        }
+    }
+
+    void ActiveAll()
+    {
+        for (int i = 0; i < food.Length; i++)
+        {
+            food[i].SetActive(true);
+        }
+    }
+    void ActivateByIndex(int index)
+    {
+        food[index].SetActive(true);
+    }
+    void DesactivateByIndex(int index)
+    {
+        food[index].SetActive(false);
     }
 }
